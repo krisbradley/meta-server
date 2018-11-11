300e68ba333f7cee96de74922038862f63b09190
Kris Bradley <kristopherbradley@gmail.com>
2018-11-10 22:41:50 -0500

    added network interface route

--- main.go
+++ main.go
@@ -3,23 +3,38 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"os"
+	"net"
 )
 
 func main() {
 	router := gin.Default()
 	router.GET("/hostname", handleHostname)
+	router.GET("/network/interface", handleNetworkInterface)
 	router.Run(":999")
 }
 
 func handleHostname(context *gin.Context) {
 	hostname, err := os.Hostname()
-	if err==nil {
-		context.JSON(200, gin.H{
-			"message": hostname,
-		})
-	} else {
-		context.JSON(500, gin.H{
-			"message": err.Error,
-		})
+	if err!=nil {
+		returnError(context, err)
 	}
+	context.JSON(200, gin.H{
+		"message": hostname,
+	})
+}
+
+func handleNetworkInterface(context *gin.Context) {
+	l, err := net.Interfaces()
+	if err != nil {
+		returnError(context, err)
+	}
+	context.JSON(200, gin.H{
+		"message": l,
+	})
+}
+
+func returnError(context *gin.Context, err error) {
+	context.JSON(500, gin.H{
+		"message": err.Error,
+	})
 }
