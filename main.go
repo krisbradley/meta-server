3e56ae02b0cf115b46150500b5cfaf33bf0830fe
Kris Bradley <kristopherbradley@gmail.com>
2018-11-11 17:39:49 -0500

    added ip address route

--- main.go
+++ main.go
@@ -1,40 +1,54 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"os"
 	"net"
+	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
 	router.GET("/hostname", handleHostname)
+	router.GET("/network", handleNetwork)
 	router.GET("/network/interface", handleNetworkInterface)
 	router.Run(":999")
 }
 
 func handleHostname(context *gin.Context) {
 	hostname, err := os.Hostname()
-	if err!=nil {
-		returnError(context, err)
-	}
-	context.JSON(200, gin.H{
-		"message": hostname,
-	})
-}
-
-func handleNetworkInterface(context *gin.Context) {
-	l, err := net.Interfaces()
 	if err != nil {
 		returnError(context, err)
 	}
-	context.JSON(200, gin.H{
-		"message": l,
-	})
+	returnOk(context, hostname)
+}
+
+func handleNetwork(context *gin.Context) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		returnError(context, err)
+	}
+	for _, address := range addrs {
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				returnOk(context, ipnet.IP.String())
+			}
+		}
+	}
+}
+
+func handleNetworkInterface(context *gin.Context) {
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		returnError(context, err)
+	}
+	returnOk(context, interfaces)
+}
+
+func returnOk(context *gin.Context, message interface{}) {
+	context.JSON(200, message)
 }
 
 func returnError(context *gin.Context, err error) {
-	context.JSON(500, gin.H{
-		"message": err.Error,
-	})
+	context.JSON(500, err.Error)
 }
