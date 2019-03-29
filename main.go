487d21eb1cd4687651d244332e5380a8304b075e
Kris Bradley <kbrbq@allstate.com>
2019-03-29 09:34:55 -0400

    Nil string dec

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func handleNetwork(context *gin.Context) {
 	if err != nil {
 		returnError(context, err)
 	}
-	ip := []string{}
+	var ip []string
 	for _, address := range addrs {
 		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
 			if ipnet.IP.To4() != nil {
